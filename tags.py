777891aadd2ac38c6fd5f178474bc8434e39fcb2
Tomi Javanainen <tomi.javanainen@gmail.com>
2025-01-10 21:22:11 +0200

    Add typing for Tags

--- tags.py
+++ tags.py
@@ -1,5 +1,6 @@
 import json
 import os
+from typing import TypedDict, NotRequired
 from urllib.parse import urljoin
 
 import dotenv
@@ -10,7 +11,13 @@ import settings
 dotenv.load_dotenv()
 
 
-def get_all_tags():
+class Tag(TypedDict):
+    name: str
+    modelCount: int
+    link: NotRequired[str]
+
+
+def get_all_tags() -> list[Tag]:
     """There are like 100k+ tags, this takes a while..."""
     url = urljoin(settings.BASE_URL, "tags")
     all_tags = []
@@ -41,12 +48,20 @@ def get_all_tags():
     return all_tags
 
 
-def save_tags_to_cache(tags):
+def save_tags_to_cache(tags: list[Tag]):
     print(f"Saving {len(tags)} tags to cache")
     with open(
         os.path.join(settings.CACHE_DIR, "tags.json"), "w", encoding="utf-8"
     ) as f:
         json.dump(tags, f, ensure_ascii=False, indent=4)
+
+
+def load_tags_from_cache() -> list[Tag]:
+    print(f"Loading tags from cache")
+    with open(
+        os.path.join(settings.CACHE_DIR, "tags.json"), "r", encoding="utf-8"
+    ) as f:
+        return json.load(f)
 
 
 if __name__ == "__main__":
